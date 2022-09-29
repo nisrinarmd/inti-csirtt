9537bb19a97543670893102d9e835908790409ba
nisrinarmd <112994152+nisrinarmd@users.noreply.github.com>
2022-09-29 13:49:50 +0700

    Update kebijakan.php

--- rfc2350/kebijakan.php
+++ rfc2350/kebijakan.php
@@ -26,6 +26,7 @@
     <!--begin::Fonts-->
     <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Poppins:300,400,500,600,700" />
     <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.1/css/bootstrap.min.css">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.2.0/css/all.min.css">
     <!-- MAIN CSS
 	<link rel="stylesheet" href="<?php echo base_url() ?>assets-landing/css/templatemo-digital-trend.css"> -->
 
@@ -64,15 +65,41 @@
             text-align: center;
             border-radius: 5px;
         }
+
+        .carousel-inner {
+            border-radius: 20px;
+        }
+
+        .carousel-item {
+            margin-top: 50px;
+        }
+
+        .carousel-item img {
+            left: 0;
+            min-width: 100%;
+            height: 474px;
+            border-radius: 50px;
+        }
+
+        .left {
+            min-width: 30%;
+            height: 50%;
+            position: absolute;
+            top: 35%;
+            left: -2%;
+            background-color: #150544;
+            border-radius: 20px;
+            z-index: 9;
+        }
     </style>
 
 </head>
 
 <!-- MENU BAR -->
-<nav class="navbar navbar-expand-lg">
+<nav class="navbar navbar-expand-lg" style="margin-top: -10px;">
 
-    <div class="container">
-        <a class="navbar-brand" href="#">
+    <div class="container" style="width: 100%; ">
+        <a class="navbar-brand" href="#" style="padding: 15px; margin-top: -10px; margin-left:0px;">
             <img src="<?php echo base_url() ?>assets/images/ini-inti.png" class="" alt="logo umprop inti" width="75">
         </a>
 
@@ -87,84 +114,63 @@
                         <a href="<?php echo site_url('Home'); ?>" class="nav-link" style="color: black;">Home</a>
                     </li>
                     <li class="nav-item">
-                        <a href="<?php echo site_url(); ?>" class="nav-link" style="color: black;">Profil</a>
+                        <a href="<?php echo site_url('Content/profil'); ?>" class="nav-link" style="color: black;">Profil</a>
                     </li>
                     <li class="nav-item">
-                        <a href="<?php echo site_url(); ?>" class="nav-link" style="color: black;">Layanan INTI-CSIRT</a>
+                        <a href="<?php echo site_url('Content/layanan'); ?>" class="nav-link" style="color: black;">Layanan INTI-CSIRT</a>
                     </li>
                     <li class="nav-item">
-                        <a href="<?php echo site_url('Content'); ?>" class="nav-link" style="color: black;">RFC 2350</a>
+                        <a href="<?php echo site_url(''); ?>" class="nav-link" style="color: black;">RFC 2350</a>
                     </li>
                     <li class="nav-item dropdown" style="margin-left:-10px;">
                         <a class="nav-link dropdown-toggle" href="" id="navbarDropdownMenuLink" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false" style="color: black;">
                         </a>
                         <ul class="dropdown-menu" aria-labelledby="navbarDropdownMenuLink">
-                            <li><a class="dropdown-item" href="#">Informasi Dokumen</a></li>
-                            <li><a class="dropdown-item" href="#">Informasi Kontak</a></li>
+                            <li><a class="dropdown-item" href="<?php echo site_url('Content/indok/'); ?>">Informasi Dokumen</a></li>
+                            <li><a class="dropdown-item" href="<?php echo site_url('Content/informasi/'); ?>">Informasi Kontak</a></li>
                             <li><a class="dropdown-item" href="<?php echo site_url('Content/tentang/'); ?>">Tentang INTI-CSIRT</a></li>
                             <li><a class="dropdown-item" href="<?php echo site_url('Content/kebijakan/'); ?>">Kebijakan</a></li>
-                            <li><a class="dropdown-item" href="#">Layanan INTI-CSIRT</a></li>
                             <li><a class="dropdown-item" href="<?php echo site_url('Content/dokpen/'); ?>">Dokumen Legal Pendukung</a></li>
+                            <li><a class="dropdown-item" href="<?php echo site_url('Content/alper/'); ?>">Alamat Pelaporan</a></li>
                         </ul>
                     </li>
                     <li class="nav-item">
                         <a href="<?php echo site_url('Content/siber'); ?>" class="nav-link" style="color: black;">Aduan Siber</a>
                     </li>
                     <li class="nav-item">
-                        <a href="<?php echo site_url(); ?>" class="nav-link" style="color: black;">Hubungi Kami</a>
+                        <a href="<?php echo site_url('Content/hubungi'); ?>" class="nav-link" style="color: black;">Hubungi Kami</a>
                     </li>
                 </ul>
             </div>
         </ul>
     </div>
 </nav>
-<div id="carouselSlider" class="carousel slide">
-    <div class="carousel-inner" style=" padding: 55px; margin-top: -54px; border: radius 10px; ">
-        <div class="carousel-item active">
-            <img src="<?php echo base_url() ?>assets/images/GDPR.jpg" width="1250" height="500" alt="First slide" class="rounded-4" style="border-radius: 25px;">
-        </div>
-        <div class="carousel-item">
-            <img src="<?php echo base_url() ?>assets/images/GLOBE.jpg" width="1250" height="500" alt="Second slide" class="rounded-4" style="border-radius: 25px;">
-        </div>
-    </div>
-    <a class="carousel-control-prev" href="#carouselSlider" role="button" data-slide="prev">
-        <span class="carousel-control-prev-icon" aria-hidden="true"></span>
-        <span class="sr-only">Previous</span>
-    </a>
-    <a class="carousel-control-next" href="#carouselSlider" role="button" data-slide="next">
-        <span class="carousel-control-next-icon" aria-hidden="true"></span>
-        <span class="sr-only">Next</span>
-    </a>
-</div>
-
 
 <!-- HERO -->
 <section class="" style="width:100%; height: 900px;">
-    <div class="container" style="background-color: #150544; width:970px; height:700px; border-top-right-radius: 40px; margin-right:35px;">
-        <h1>Kebijakan</h1>
-        <p style="color: white; font-family:poppins; margin: right 50%; margin-left:420px; margin-top:60px;"><img src="<?php echo base_url(); ?>assets/images/dokpen.png" style="width:400px; margin-left: -530px; margin-top:-90px;">
+    <div class="container" style="background-color: #150544; width:80%; height:700px; border-top-right-radius: 40px; margin-right:120px; margin-top:80px;">
+        <h1 style="border-style: solid; width:22%; padding: 20px; border-radius:20px; color:white; margin-left:100px; margin-top:35px;">Kebijakan</h1>
+        <p style="color: white; font-family:poppins; margin: right 50%; margin-left:420px; margin-top:60px;"><img src="<?php echo base_url(); ?>assets/images/kebijakan.png" style="width:400px; margin-left: -530px; margin-top:-90px;">
         </p>
         <p style="color: white; font-family:poppins; margin: right 50%; margin-left:350px; margin-top:-430px;">
         <h5>INTI-CSIRT Akan Membantu Konstituen Untuk Melakukan Penanggulangan Dan Pemulihan Insiden Keamanan <br>Siber Di Lingkungan PT INTI (Persero) Dengan <br>Aspek-Aspek Manajemen Insiden Keamanan Siber Berikut :
         </h5><br>
-        <h4>1.Jenis-jenis Insiden dan Tingkat/Level Dukungan</h4>
-        <h5>INTI-CSIRTmemiliki otoritas untuk menangani insiden yaitu :
+        <h4 style="color:white; font-family: poppins; margin-left:440px; margin-top:10px;"><b style="font-size: 35px;">1.</b> Jenis-jenis Insiden dan Tingkat/Level Dukungan</h4>
+        <h5 style="margin-left:470px; margin-top:10px;">INTI-CSIRTmemiliki otoritas untuk menangani insiden yaitu :
             <br>1. Web Defacement;
             <br>2. DDOS;
             <br>3. Malware;
             <br>4. Phising;
             <br>Dukungan yang diberikan oleh INTI-CSIRT <br>kepada konstituen dapat bervariasi bergantung <br>dari jenis dan dampak insiden.
-        </h5><br>
-        <h4>2.Kerja sama, Interaksi dan Pengungkapan <br>Informasi/data</h4>
-        <h5>INTI-CSIRT akan melakukan kerjasama dan <br>berbagi informasi dengan CSIRT atau organisasi <br>lainnya dalam lingkup keamanan siber. Seluruh <br>informasi yang diterima oleh INTI-CSIRT akan <br>dirahasiakan.</h5>
-        <h4>3.Komunikasi dan Autentikasi</h4><br>
-        <h5>Untuk komunikasi biasa INTI-CSIRT dapat <br>menggunakan alamat e-mail tanpa enkripsi data <br>(e-mail konvensional) dan telepon. Namun, untuk <br>komunikasi yang memuat informasi sensitif/terbatas/<br>rahasia dapat menggunakan enkripsi PGP pada e-mail.</h5>
+        </h5>
+        <h4 style="color:white; font-family: poppins; margin-left:440px; margin-top:10px;"><b style="font-size: 35px;">2.</b> Kerja sama, Interaksi dan Pengungkapan Informasi/data</h4>
+        <h5 style="margin-left:470px; margin-top:10px;">INTI-CSIRT akan melakukan kerjasama dan <br>berbagi informasi dengan CSIRT atau organisasi <br>lainnya dalam lingkup keamanan siber. Seluruh <br>informasi yang diterima oleh INTI-CSIRT akan <br>dirahasiakan.</h5>
+        <h4 style="color:white; font-family: poppins; margin-left:440px; margin-top:10px;"><b style="font-size: 35px;">3.</b> Komunikasi dan Autentikasi</h4>
+        <h5 style="margin-left:470px; margin-top:10px;">Untuk komunikasi biasa INTI-CSIRT dapat <br>menggunakan alamat e-mail tanpa enkripsi data <br>(e-mail konvensional) dan telepon. Namun, untuk <br>komunikasi yang memuat informasi sensitif/terbatas/<br>rahasia dapat menggunakan enkripsi PGP pada e-mail.</h5>
     </div>
 </section>
-<footer>
-    <div class="footer">
-        Copyright &copy; 2022 INTI-CSIRT
-    </div>
+<footer style="background:#150544; padding:10px; text-align:center; height:100px;">
+    <p style="margin-top: 28px; color:white; font-family:poppins;">Copyright &copy; 2022 INTI-CSIRT</p>
 </footer>
 </body>
 
