c074ebbadd3d2c27a03a662ba6d019ca0c9bc9fd
nisrinarmd <112994152+nisrinarmd@users.noreply.github.com>
2022-09-29 13:50:29 +0700

    Update tentang-csirt.php

--- rfc2350/tentang-csirt.php
+++ rfc2350/tentang-csirt.php
@@ -19,33 +19,98 @@
     <link rel="stylesheet" href="<?php echo base_url() ?>assets-landing/css/aos.css">
     <link rel="stylesheet" href="<?php echo base_url() ?>assets-landing/css/owl.carousel.min.css">
     <link rel="stylesheet" href="<?php echo base_url() ?>assets-landing/css/owl.theme.default.min.css">
-    <link rel="stylesheet" href="<?php echo base_url() ?>assets-landing/css/ttg-csirt.css">
 
     <!-- Style.css -->
-    <!-- <link rel="stylesheet" type="text/css" href="<?php echo base_url(); ?>assets\css\style.css"> -->
+    <link rel="stylesheet" type="text/css" href="<?php echo base_url(); ?>assets\css\style.css">
 
     <!--begin::Fonts-->
     <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Poppins:300,400,500,600,700" />
     <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.1/css/bootstrap.min.css">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.2.0/css/all.min.css">
     <!-- MAIN CSS
 	<link rel="stylesheet" href="<?php echo base_url() ?>assets-landing/css/templatemo-digital-trend.css"> -->
 
     <!-- SCRIPTS -->
     <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.6.0/jquery.min.js"></script>
     <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.1/js/bootstrap.min.js"></script>
-    <!-- <script src="<?php echo base_url() ?>assets-landing/js/jquery.min.js"></script>
-	<script src="<?php echo base_url() ?>assets-landing/js/bootstrap.min.js"></script> -->
-    <!-- <script src="<?php echo base_url() ?>assets-landing/js/aos.js"></script> -->
+    <script src="<?php echo base_url() ?>assets-landing/js/jquery.min.js"></script>
+    <script src="<?php echo base_url() ?>assets-landing/js/bootstrap.min.js"></script>
+    <script src="<?php echo base_url() ?>assets-landing/js/aos.js"></script>
     <script src="<?php echo base_url() ?>assets-landing/js/owl.carousel.min.js"></script>
     <script src="<?php echo base_url() ?>assets-landing/js/smoothscroll.js"></script>
-    <!-- <script src="<?php echo base_url() ?>assets-landing/js/custom.js"></script> -->
+    <script src="<?php echo base_url() ?>assets-landing/js/custom.js"></script>
+
+    <style>
+        h2 {
+            font-family: poppins;
+            text-align: left;
+            color: white;
+            margin-left: 380px;
+            margin-top: 0px;
+        }
+
+        h4 {
+            font-family: poppins;
+            text-align: left;
+            color: white;
+            margin-left: 380px;
+            margin-top: 0px;
+        }
+
+        h5 {
+            font-family: poppins;
+            text-align: left;
+            color: white;
+            margin-left: 400px;
+            margin-top: 0px;
+        }
+
+        footer {
+            height: 100px;
+            background-color: #150544;
+            color: white;
+            font-family: poppins;
+            text-align: center;
+            border-radius: 5px;
+        }
+
+        b {
+            margin-left: 0px;
+        }
+
+        .carousel-inner {
+            border-radius: 20px;
+        }
+
+        .carousel-item {
+            margin-top: 50px;
+        }
+
+        .carousel-item img {
+            left: 0;
+            min-width: 100%;
+            height: 474px;
+            border-radius: 50px;
+        }
+
+        .left {
+            min-width: 30%;
+            height: 50%;
+            position: absolute;
+            top: 35%;
+            left: -2%;
+            background-color: #150544;
+            border-radius: 20px;
+            z-index: 9;
+        }
+    </style>
 </head>
 
 <!-- MENU BAR -->
-<nav class="navbar navbar-expand-lg">
+<nav class="navbar navbar-expand-lg" style="margin-top: -10px;">
 
-    <div class="container">
-        <a class="navbar-brand" href="#">
+    <div class="container" style="width: 100%; ">
+        <a class="navbar-brand" href="#" style="padding: 15px; margin-top: -10px; margin-left:0px;">
             <img src="<?php echo base_url() ?>assets/images/ini-inti.png" class="" alt="logo umprop inti" width="75">
         </a>
 
@@ -60,86 +125,72 @@
                         <a href="<?php echo site_url('Home'); ?>" class="nav-link" style="color: black;">Home</a>
                     </li>
                     <li class="nav-item">
-                        <a href="<?php echo site_url(); ?>" class="nav-link" style="color: black;">Profil</a>
+                        <a href="<?php echo site_url('Content/profil'); ?>" class="nav-link" style="color: black;">Profil</a>
                     </li>
                     <li class="nav-item">
-                        <a href="<?php echo site_url(); ?>" class="nav-link" style="color: black;">Layanan INTI-CSIRT</a>
+                        <a href="<?php echo site_url('Content/layanan'); ?>" class="nav-link" style="color: black;">Layanan INTI-CSIRT</a>
                     </li>
                     <li class="nav-item">
-                        <a href="<?php echo site_url('Content'); ?>" class="nav-link" style="color: black;">RFC 2350</a>
+                        <a href="<?php echo site_url(''); ?>" class="nav-link" style="color: black;">RFC 2350</a>
                     </li>
                     <li class="nav-item dropdown" style="margin-left:-10px;">
                         <a class="nav-link dropdown-toggle" href="" id="navbarDropdownMenuLink" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false" style="color: black;">
                         </a>
                         <ul class="dropdown-menu" aria-labelledby="navbarDropdownMenuLink">
-                            <li><a class="dropdown-item" href="#">Informasi Dokumen</a></li>
-                            <li><a class="dropdown-item" href="#">Informasi Kontak</a></li>
+                            <li><a class="dropdown-item" href="<?php echo site_url('Content/indok/'); ?>">Informasi Dokumen</a></li>
+                            <li><a class="dropdown-item" href="<?php echo site_url('Content/informasi/'); ?>">Informasi Kontak</a></li>
                             <li><a class="dropdown-item" href="<?php echo site_url('Content/tentang/'); ?>">Tentang INTI-CSIRT</a></li>
                             <li><a class="dropdown-item" href="<?php echo site_url('Content/kebijakan/'); ?>">Kebijakan</a></li>
-                            <li><a class="dropdown-item" href="#">Layanan INTI-CSIRT</a></li>
-                            <li><a class="dropdown-item" href="#">Dokumen Legal Pendukung</a></li>
+                            <li><a class="dropdown-item" href="<?php echo site_url('Content/dokpen/'); ?>">Dokumen Legal Pendukung</a></li>
+                            <li><a class="dropdown-item" href="<?php echo site_url('Content/alper/'); ?>">Alamat Pelaporan</a></li>
                         </ul>
                     </li>
                     <li class="nav-item">
                         <a href="<?php echo site_url('Content/siber'); ?>" class="nav-link" style="color: black;">Aduan Siber</a>
                     </li>
                     <li class="nav-item">
-                        <a href="<?php echo site_url(); ?>" class="nav-link" style="color: black;">Hubungi Kami</a>
+                        <a href="<?php echo site_url('Content/hubungi'); ?>" class="nav-link" style="color: black;">Hubungi Kami</a>
                     </li>
                 </ul>
             </div>
         </ul>
     </div>
 </nav>
-<div id="carouselSlider" class="carousel slide">
-    <div class="carousel-inner" style=" padding: 55px; margin-top: -54px; border: radius 10px; ">
-        <div class="carousel-item active">
-            <img src="<?php echo base_url() ?>assets/images/GDPR.jpg" width="1250" height="500" alt="First slide" class="rounded-4" style="border-radius: 25px;">
-        </div>
-        <div class="carousel-item">
-            <img src="<?php echo base_url() ?>assets/images/GLOBE.jpg" width="1250" height="500" alt="Second slide" class="rounded-4" style="border-radius: 25px;">
-        </div>
-    </div>
-    <a class="carousel-control-prev" href="#carouselSlider" role="button" data-slide="prev">
-        <span class="carousel-control-prev-icon" aria-hidden="true"></span>
-        <span class="sr-only">Previous</span>
-    </a>
-    <a class="carousel-control-next" href="#carouselSlider" role="button" data-slide="next">
-        <span class="carousel-control-next-icon" aria-hidden="true"></span>
-        <span class="sr-only">Next</span>
-    </a>
-</div>
-<section class="content">
-    <div class="container1">
-        <h1 style="margin-left:150px; color:white;"><br>TENTANG INTI-CSIRT</h1><br><br>
-        <div class="parag"><br>
-            <p>Dokumen ini berisi deskripsi INTI-CSIRT berdasarkan RFC 2350, yaitu <br>informasi dasar mengenai INTI-CSIRT, menjelaskan tanggung <br>jawab, layanan yang diberikan, dan cara untuk menghubungi INTI-<br>CSIRT.</p>
-            <p><b>
-                    <h2>1.Visi</h2>
-                </b> Visi INTI-CSIRT adalah menjadi komponen utama untuk <br>tercapainya Visi Perusahaan dengan meningkatkan <br>ketahanan siber di sektor industri telekomunikasi.</p>
-            <p><b>
-                    <h2>2.Misi</h2>
-                </b>Perwujudan visi sebagaimana dituangkan di atas akan <br>dicapai melalui upaya- upaya yang terkandung dalam <br>misi INTI-CSIRT, yaitu :
-                <br>1. Mengkoordinasikan dan mengkolaborasikan layanan <br>keamanan siber di lingkungan Perusahaan dan <br>pemangku kepentingan.
-                <br>2. Membangun kemampuan dan kapasitas sumber daya <br>keamanan di sektor industri BUMN.
-                <br>3. Membangun kerjasama dalam rangka <br>penanggulangan dan pemulihan insiden keamanan siber <br>di lingkungan sektor industri BUMN.
-            </p>
-            <p><b>
-                    <h2>3.Konstituen</h2>
-                </b>Konstituen INTI-CSIRT meliputi seluruh pengguna yang <br>menggunakan layanan Data Center Perusahaan <br>Perseroan (Persero) PT Industri Telekomunikasi Indonesia, <br>meliputi karyawan dan anak perusahaan, yaitu PT. Inti <br>Konten Indonesia (INTENS) dan PT. Inti Pindad Mitra Sejati <br>(IPMS).</p>
-            <p><b>
-                    <h2>4.Sponsorship dan/atau Afiliasi</h2>
-                </b>Pendanaan INTI-CSIRT bersumber dari anggaran <br>pemeliharaan infrastrukutur Teknologi Informasi <br>Perusahaan Perseroan (Persero) PT Industri <br>Telekomunikasi Indonesia yang sudah dianggarkan setiap <br>tahunnya.</p>
-            <p><b>
-                    <h2>5.Otoritas</h2>
-                </b><br> Tim INTI-CSIRT memiliki kewenangan antara lain:
-                <br>1. Menentukan asesmen tingkat keamanan informasi pada <br>proses bisnis yang sedang atau yang akan berlangsung;
-                <br>2. Melakukan asesmen tingkat keamanan sistem informasi <br>yang dibuat secara sendiri (in-house), atau disewa/dibeli <br>ke pihak ketiga;
-                <br>3. Melakukan pengawasan serta intervensi aktif terhadap <br>operasional sistem informasi dalam rangka pemenuhan <br>ketahanan dan keandalan siber yang menunjang tujuan <br>bisnis;
-                <br>4. Merencanakan, membuat dan mengoperasikan <br>rancang bangun mekanisme pertahanan berlapis <br>siber (cyber defense-in-depth);
-                <br>5. Melaksanakan program kesadaran keamanan siber <br>bersama stakeholder terkait;
-                <br>6. Memiliki otoritas penuh untuk melaksanakan koordinasi <br>dan intervensi internal dan eksternal, akses terhadap data <br>dan sistem dalam hal penanganan insiden siber Anti- <br>Malware.
-            </p>
-        </div>
+
+<!-- HERO -->
+<section class="" style="width:100%; height: 1560px;">
+    <div class="container" style="background-color: #150544; width:1060px; height:1300px; border-top-right-radius: 40px; margin-right:120px; margin-top:80px;">
+        <h2 style="border-style: solid; width:31%; padding: 10px; border-radius:20px; color:white; margin-left:100px; margin-top:35px;">Tentang INTI-CSIRT</h2>
+        <p style=" color: white; font-family:poppins; margin: right 50%; margin-left:420px; margin-top:70px;"><img src="<?php echo base_url(); ?>assets/images/logo-gembok.png" style="width:400px; margin-left: -530px; margin-top:-90px;">
+        </p>
+        <p style="color: white; font-family:poppins; margin: right 50%; margin-left:400px; margin-top:-340px;">
+        <h4>Dokumen ini berisi deskripsi INTI-CSIRT berdasarkan RFC 2350, yaitu informasi dasar mengenai INTI-CSIRT, menjelaskan tanggung <br>jawab, layanan yang diberikan, dan cara untuk menghubungi INTI-<br>CSIRT.</h4><br>
+        </p>
+        <h2><b style="font-size: 40px;">1.</b> Visi</h2>
+        <h5>Visi INTI-CSIRT adalah menjadi komponen utama untuk <br>tercapainya Visi Perusahaan dengan meningkatkan <br>ketahanan siber di sektor industri telekomunikasi.</h5><br>
+        <h2><b style="font-size: 40px;">2.</b> Misi</h2>
+        <h5>Perwujudan visi sebagaimana dituangkan di atas akan <br>dicapai melalui upaya- upaya yang terkandung dalam <br>misi INTI-CSIRT,
+            yaitu : <br>1. Mengkoordinasikan dan mengkolaborasikan layanan <br>keamanan siber di lingkungan Perusahaan dan <br>pemangku kepentingan. <br>2. Membangun kemampuan dan kapasitas sumber daya <br>keamanan di sektor industri BUMN. <br>3. Membangun kerjasama dalam rangka <br>penanggulangan dan pemulihan insiden keamanan siber <br>di lingkungan sektor industri BUMN.</h5><br>
+        <h2><b style="font-size: 40px;">3.</b> Konstituen</h2>
+        <h5>Konstituen INTI-CSIRT meliputi seluruh pengguna yang <br>menggunakan layanan Data Center Perusahaan <br>Perseroan (Persero) PT Industri Telekomunikasi Indonesia,<br>meliputi karyawan dan anak perusahaan,yaitu PT. Inti <br>Konten Indonesia (INTENS) dan PT. Inti Pindad Mitra Sejati <br>(IPMS).</h5><br>
+        <h2><b style="font-size: 40px;">4.</b> Sponsorship dan/atau Afiliasi</h2>
+        <h5>Pendanaan INTI-CSIRT bersumber dari anggaran <br>pemeliharaan infrastrukutur Teknologi Informasi <br>Perusahaan Perseroan (Persero) PT Industri <br>Telekomunikasi Indonesia yang sudah dianggarkan setiap <br>tahunnya.</h5>
+        <h2><b style="font-size: 40px;">5.</b> Otoritas</h2>
+        <h5>Tim INTI-CSIRT memiliki kewenangan antara lain:
+            <br>1. Menentukan asesmen tingkat keamanan informasi pada <br>proses bisnis yang sedang atau yang akan berlangsung;
+            <br>2. Melakukan asesmen tingkat keamanan sistem informasi <br>yang dibuat secara sendiri (in-house),
+            atau disewa/dibeli <br>ke pihak ketiga;
+            <br>3. Melakukan pengawasan serta intervensi aktif terhadap <br>operasional sistem informasi dalam rangka pemenuhan <br>ketahanan dan keandalan siber yang menunjang tujuan <br>bisnis;
+            <br>4. Merencanakan,membuat dan mengoperasikan <br>rancang bangun mekanisme pertahanan berlapis <br>siber (cyber defense-in-depth);
+            <br>5. Melaksanakan program kesadaran keamanan siber <br>bersama stakeholder terkait;
+            <br>6. Memiliki otoritas penuh untuk melaksanakan koordinasi <br>dan intervensi internal dan eksternal,
+            akses terhadap data <br>dan sistem dalam hal penanganan insiden siber Anti – <br>Malware.
+        </h5>
     </div>
 </section>
+<footer style="background:#150544; padding:10px; text-align:center; height:100px;">
+    <p style="margin-top: 28px; color:white; font-family:poppins;">Copyright &copy; 2022 INTI-CSIRT</p>
+</footer>
+</body>
+
+</html>
