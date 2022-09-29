2e8442f8900dc60d5bdfa0b9276a957d102fd5f4
nisrinarmd <112994152+nisrinarmd@users.noreply.github.com>
2022-09-29 13:47:37 +0700

    Update informasi-dokumen.php

--- rfc2350/informasi-dokumen.php
+++ rfc2350/informasi-dokumen.php
@@ -19,12 +19,14 @@
     <link rel="stylesheet" href="<?php echo base_url() ?>assets-landing/css/aos.css">
     <link rel="stylesheet" href="<?php echo base_url() ?>assets-landing/css/owl.carousel.min.css">
     <link rel="stylesheet" href="<?php echo base_url() ?>assets-landing/css/owl.theme.default.min.css">
+
     <!-- Style.css -->
     <link rel="stylesheet" type="text/css" href="<?php echo base_url(); ?>assets\css\style.css">
 
     <!--begin::Fonts-->
     <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Poppins:300,400,500,600,700" />
     <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.1/css/bootstrap.min.css">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.2.0/css/all.min.css">
     <!-- MAIN CSS
 	<link rel="stylesheet" href="<?php echo base_url() ?>assets-landing/css/templatemo-digital-trend.css"> -->
 
@@ -39,21 +41,35 @@
     <script src="<?php echo base_url() ?>assets-landing/js/custom.js"></script>
 
     <style>
-        html,
-        body {
-            margin: 0;
-            height: 100%;
-            width: 100%;
-            padding: 0;
+        .container {
+            border-radius: 20px;
+            position: relative;
         }
 
-        section {
-            display: block;
-            height: 100%;
-            width: 100%;
-            padding: 30px;
-            padding-left: 0px;
-            box-sizing: border-box;
+        .carousel-inner {
+            border-radius: 20px;
+        }
+
+        .carousel-item {
+            margin-top: 50px;
+        }
+
+        .carousel-item img {
+            left: 0;
+            min-width: 100%;
+            height: 474px;
+            border-radius: 50px;
+        }
+
+        .left {
+            min-width: 30%;
+            height: 50%;
+            position: absolute;
+            top: 35%;
+            left: -2%;
+            background-color: #150544;
+            border-radius: 20px;
+            z-index: 9;
         }
     </style>
 </head>
@@ -74,124 +90,77 @@
             <div class="collapse navbar-collapse" id="navbarNav" style="padding-right: -60px; font-family:poppins;">
                 <ul class="navbar-nav ml-auto">
                     <li class="nav-item">
-                        <a href="<?php echo site_url('Home'); ?>" class="nav-link">Home</a>
+                        <a href="<?php echo site_url('Home'); ?>" class="nav-link" style="color: black;">Home</a>
                     </li>
                     <li class="nav-item">
-                        <a href="<?php echo site_url('Content/profil'); ?>" class="nav-link">Profil</a>
+                        <a href="<?php echo site_url('Content/profil'); ?>" class="nav-link" style="color: black;">Profil</a>
                     </li>
                     <li class="nav-item">
-                        <a href="<?php echo site_url(); ?>" class="nav-link">Layanan INTI-CSIRT</a>
+                        <a href="<?php echo site_url('Content/layanan'); ?>" class="nav-link" style="color: black;">Layanan INTI-CSIRT</a>
                     </li>
                     <li class="nav-item">
-                        <a href="<?php echo site_url('Content'); ?>" class="nav-link">RFC 2350</a>
+                        <a href="<?php echo site_url(''); ?>" class="nav-link" style="color: black;">RFC 2350</a>
                     </li>
                     <li class="nav-item dropdown" style="margin-left:-10px;">
-                        <a class="nav-link dropdown-toggle" href="" id="navbarDropdownMenuLink" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+                        <a class="nav-link dropdown-toggle" href="" id="navbarDropdownMenuLink" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false" style="color: black;">
                         </a>
                         <ul class="dropdown-menu" aria-labelledby="navbarDropdownMenuLink">
                             <li><a class="dropdown-item" href="<?php echo site_url('Content/indok/'); ?>">Informasi Dokumen</a></li>
-                            <li><a class="dropdown-item" href="#">Informasi Kontak</a></li>
+                            <li><a class="dropdown-item" href="<?php echo site_url('Content/informasi/'); ?>">Informasi Kontak</a></li>
                             <li><a class="dropdown-item" href="<?php echo site_url('Content/tentang/'); ?>">Tentang INTI-CSIRT</a></li>
                             <li><a class="dropdown-item" href="<?php echo site_url('Content/kebijakan/'); ?>">Kebijakan</a></li>
-                            <li><a class="dropdown-item" href="#">Layanan INTI-CSIRT</a></li>
-                            <li><a class="dropdown-item" href="#">Dokumen Legal Pendukung</a></li>
+                            <li><a class="dropdown-item" href="<?php echo site_url('Content/dokpen/'); ?>">Dokumen Legal Pendukung</a></li>
+                            <li><a class="dropdown-item" href="<?php echo site_url('Content/alper/'); ?>">Alamat Pelaporan</a></li>
                         </ul>
                     </li>
                     <li class="nav-item">
-                        <a href="<?php echo site_url('Content/siber'); ?>" class="nav-link">Aduan Siber</a>
+                        <a href="<?php echo site_url('Content/siber'); ?>" class="nav-link" style="color: black;">Aduan Siber</a>
                     </li>
                     <li class="nav-item">
-                        <a href="<?php echo site_url(); ?>" class="nav-link">Hubungi Kami</a>
+                        <a href="<?php echo site_url('Content/hubungi'); ?>" class="nav-link" style="color: black;">Hubungi Kami</a>
                     </li>
                 </ul>
             </div>
         </ul>
     </div>
 </nav>
-<div id="carouselSlider" class="carousel slide">
-    <div class="carousel-inner" style=" padding: 55px; margin-top: -54px; border: radius 10px; ">
-        <div class="carousel-item active">
-            <img src="<?php echo base_url() ?>assets/images/GDPR.jpg" width="1250" height="500" alt="First slide" class="rounded-4" style="border-radius: 25px;">
-        </div>
-        <div class="carousel-item">
-            <img src="<?php echo base_url() ?>assets/images/GLOBE.jpg" width="1250" height="500" alt="Second slide" class="rounded-4" style="border-radius: 25px;">
-        </div>
-    </div>
-    <a class="carousel-control-prev" href="#carouselSlider" role="button" data-slide="prev">
-        <span class="carousel-control-prev-icon" aria-hidden="true"></span>
-        <span class="sr-only">Previous</span>
-    </a>
-    <a class="carousel-control-next" href="#carouselSlider" role="button" data-slide="next">
-        <span class="carousel-control-next-icon" aria-hidden="true"></span>
-        <span class="sr-only">Next</span>
-    </a>
-</div>
 
-
-<!-- HERO -->
-<section class="rounded-4" style="background-color: #150544; height:1500px; width:75%; border-top-right-radius: 50px; margin-left:250px;">
-    <div class="container" style="width: 100%;">
-        <h2 style="color:white; font-family: poppins; margin-left:100px;">INFORMASI DOKUMEN</h2>
-        <br><br>
-        <p style="color:white; font-family: poppins; margin-left:450px;">Dokumen ini berisi deskripsi INTI-CSIRT berdasarkan RFC 2350, yaitu informasi dasar mengenai INTI-CSIRT, menjelaskan tanggung jawab, layanan yang diberikan, dan cara untuk menghubungi INTI-CSIRT.
-        <p>
-        <h3 style="color:white; font-family: poppins; margin-left:450px;">1. Tanggal Update Terakhir
-        </h3>
-        <h4 style="color:white; font-family: poppins; margin-left:450px;">
-            Dokumen merupakan dokumen versi 1.0 yang diterbitkan pada tanggal 8 Agustus 2022.
-        </h4>
-        <br><br>
-        <h3 style="color:white; font-family: poppins; margin-left:450px;">2. Daftar Distribusi untuk Pemberitahuan
-        </h3>
-        <p style="color:white; font-family: poppins; margin-left:450px;">
-            Tidak ada daftar distribusi untuk pemberitahuan mengenai pembaharuan dokumen.
-        <p>
-            <br><br>
-        <h3 style="color:white; font-family: poppins; margin-left:450px;">3. Lokasi dimana Dokumen ini bisa didapat
-        </h3>
-        <p style="color:white; font-family: poppins; margin-left:450px;">
-            Dokumen in tersedia pada link berikut :
-            <br>Dokumen RFC 2350
-            <br>QR Code
-        <p>
-            <br><br>
-        <h3 style="color:white; font-family: poppins; margin-left:450px;">4. Keaslian Dokumen
-        </h3>
-        <p style="color:white; font-family: poppins; margin-left:450px;">
-            Dokumen ini telah ditandatangani dengan PGP milik INTI-CSIRT. Untuk lebih jelas dapat dilihat pad bagian Kunci Publik dan Informasi / Data Enkripsi Lain.
-        <p>
-            <br><br>
-        <h3 style="color:white; font-family: poppins; margin-left:450px;">5. Identifikasi Dokumen
-        </h3>
-        <p style="color:white; font-family: poppins; margin-left:450px;">
-            Dokumen memiliki atribut, yaitu :
+< <!-- HERO -->
+    <section class="rounded-4" style="background-color: #150544; height:1400px; width:80%; border-top-right-radius: 50px; margin-left:160px; margin-top:80px;">
+        <div class="container" style="width: 100%;">
+            <h2 style="color:white; font-family: poppins; margin-left:100px; border-style: solid; width:35%; padding: 20px; border-radius:20px; color:white; margin-left:100px; margin-top:35px;">INFORMASI DOKUMEN</h2>
+            <p style="color: white; font-family:poppins; margin: right 50%; margin-left:420px; margin-top:100px;"><img src="<?php echo base_url() ?>assets/images/indok.png" style="width:440px; margin-left: -510px; margin-top:-90px;">
+            </p>
+            <h3 style="color:white; font-family: poppins; margin-left:450px; margin-top:-400px;">Dokumen ini berisi deskripsi INTI-CSIRT berdasarkan RFC 2350, yaitu informasi dasar mengenai INTI-CSIRT, menjelaskan tanggung jawab, layanan yang diberikan, dan cara untuk menghubungi INTI-CSIRT.<br>
+            </h3>
             <br>
-            Judul : RFC 2350 INTI-CSIRT;
-            <br>
-            Versi : L1;
-            <br>
-            Tanggal Publikasi : 8 Agustus 2022
-            <br>
-            Kedaluwarsa : Dokumen ini valid hingga dokumen terbaru dipublikasikan.
-        <p>
-    </div>
-</section>
-<section class="" style="background-color: white; ">
-    <div class="container">
-
-    </div>
-</section>
-<footer class=" site-footer" style="background-color: #150544;">
-    <div id="bottom-footer">
-        <div class="tg-container">
-            <div class="mx-lg-auto text-center col-12" style="background-color: #150544; height: 50px;">
-                <p class="copyright-text" style="color:white; margin-top:px;">
-                    Copyright &copy; 2022 <a href="https://csirt.inti.co.id/">INTI-CSIRT
-                </p>
-            </div>
+            <h3 style="color:white; font-family: poppins; margin-left:500px; margin-top:30px;"><b style="font-size: 45px;">1.</b> Tanggal Update Terakhir</h3>
+            <p style="color:white; font-family: poppins; margin-left:542px; margin-top:20px;">Dokumen merupakan dokumen versi 1.0 yang diterbitkan<br>pada tanggal 8 Agustus 2022.</p>
+            <h3 style="color:white; font-family: poppins; margin-left:500px; margin-top:30px;"><b style="font-size: 45px;">2.</b> Daftar Distribusi untuk Pemberitahuan</h3>
+            <p style="color:white; font-family: poppins; margin-left:542px; margin-top:20px;">Tidak ada daftar distribusi untuk pemberitahuan mengenai pembaharuan dokumen.</p>
+            <h3 style="color:white; font-family: poppins; margin-left:500px; margin-top:30px;"><b style="font-size: 45px;">3.</b> Lokasi dimana Dokumen ini bisa didapat</h3>
+            <p style="color:white; font-family: poppins; margin-left:542px; margin-top:20px;">Dokumen ini tersedia pada link berikut : </p>
+            <p style="color: #23BBD1; font-family: poppins; margin-left:542px; margin-top:10px;">Dokumen RFC 2350</p>
+            <img src="<?php echo base_url() ?>assets/images/scan.png" width="100px" alt="" style="margin-left:542px; margin-top:10px;">
+            <h3 style="color:white; font-family: poppins; margin-left:500px; margin-top:30px;"><b style="font-size: 45px;">4.</b> Keaslian Dokumen</h3>
+            <p style="color:white; font-family: poppins; margin-left:542px; margin-top:20px;">Dokumen ini telah ditandatangani dengan PGP milik INTI-CSIRT. Untuk lebih jelas dapat dilihat pada bagian Kunci Publik dan Informasi /Data Enkripsi Lain.</p>
+            <h3 style="color:white; font-family: poppins; margin-left:500px; margin-top:30px;"><b style="font-size: 45px;">5.</b> Identifikasi Dokumen</h3>
+            <p style="color:white; font-family: poppins; margin-left:542px; margin-top:20px;">Dokumen memiliki atribut, yaitu :
+                <br>Judul&emsp;: RFC 2350 INTI-CSIRT;
+                <br>Versi&emsp;&ensp;: 1.1;
+                <br>Tanggal Publikasi&ensp;: 8 Agustus 2022
+                <br>Kedaluwarsa&ensp;: Dokumen ini valid hingga dokumen terbaru dipublikasikan.
+            </p>
         </div>
-    </div>
-</footer>
-</body>
+    </section>
+    <section class="" style=" background-color: white; height:100px;">
+        <div class="container">
+
+        </div>
+    </section>
+    <footer style="background:#150544; padding:10px; text-align:center; height:100px;">
+        <p style="margin-top: 28px; color:white; font-family:poppins;">Copyright &copy; 2022 INTI-CSIRT</p>
+    </footer>
+    </body>
 
 </html>
