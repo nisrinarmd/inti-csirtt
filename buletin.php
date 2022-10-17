a14a9e2558f2d551984a071d5d843dff0698c888
nisrinarmd <112994152+nisrinarmd@users.noreply.github.com>
2022-10-17 08:48:10 +0700

    Add files via upload

---
+++ buletin.php
@@ -0,0 +1,330 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <title>INTI CSIRT</title>
+
+    <meta name="description" content="IMSIS | PT Industri Telekomunikasi Indoenesia" />
+    <meta name="viewport" content="width=device-width, initial-scale=1" />
+    <meta charset="utf-8" />
+    <meta property="og:locale" content="en_US" />
+    <meta property="og:type" content="article" />
+    <meta property="og:title" content="IMSIS | PT Industri Telekomunikasi Indoenesia" />
+    <meta property="og:url" content="https://my.inti.co.id/reseller/" />
+    <meta property="og:site_name" content="IMSIS | PT Industri Telekomunikasi Indoenesia" />
+    <link rel="shortcut icon" href="<?php echo base_url(); ?>assets/images/ini-inti1.png" />
+
+    <link rel="stylesheet" href="<?php echo base_url() ?>assets-landing/css/bootstrap.min.css">
+    <link rel="stylesheet" href="<?php echo base_url() ?>assets-landing/css/font-awesome.min.css">
+    <link rel="stylesheet" href="<?php echo base_url() ?>assets-landing/css/aos.css">
+    <link rel="stylesheet" href="<?php echo base_url() ?>assets-landing/css/owl.carousel.min.css">
+    <link rel="stylesheet" href="<?php echo base_url() ?>assets-landing/css/owl.theme.default.min.css">
+    <link rel="stylesheet" href="<?php echo base_url() ?>assets-landing/css/view-landing.css">
+    <!-- Style.css -->
+    <link rel="stylesheet" type="text/css" href="<?php echo base_url(); ?>assets\css\style.css">
+
+    <!--begin::Fonts-->
+    <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Poppins:300,400,500,600,700" />
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.1/css/bootstrap.min.css">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.2.0/css/all.min.css">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.1/dist/css/bootstrap.min.css">
+    <!-- MAIN CSS
+	<link rel="stylesheet" href="<?php echo base_url() ?>assets-landing/css/templatemo-digital-trend.css"> -->
+
+    <!-- SCRIPTS -->
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.6.0/jquery.min.js"></script>
+    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.1/js/bootstrap.min.js"></script>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.1/dist/js/bootstrap.bundle.min.js"></script>
+    <script src="<?php echo base_url() ?>assets-landing/js/jquery.min.js"></script>
+    <script src="<?php echo base_url() ?>assets-landing/js/bootstrap.min.js"></script>
+    <script src="<?php echo base_url() ?>assets-landing/js/aos.js"></script>
+    <script src="<?php echo base_url() ?>assets-landing/js/owl.carousel.min.js"></script>
+    <script src="<?php echo base_url() ?>assets-landing/js/smoothscroll.js"></script>
+    <script src="<?php echo base_url() ?>assets-landing/js/custom.js"></script>
+
+
+    <style>
+        body {
+            width: 100%;
+            height: 100%;
+        }
+
+        .carousel-inner {
+            border-radius: 20px;
+        }
+
+        .carousel-item {
+            margin-top: 50px;
+        }
+
+        .carousel-item img {
+            left: 0;
+            min-width: 100%;
+            height: 474px;
+            border-radius: 50px;
+        }
+
+        .left {
+            min-width: 30%;
+            height: 50%;
+            position: absolute;
+            top: 35%;
+            left: -2%;
+            background-color: #150544;
+            border-radius: 20px;
+            z-index: 9;
+        }
+
+        .center {
+            min-width: 28%;
+            height: 20%;
+            position: absolute;
+            top: 75%;
+            left: 40px;
+            background-color: #FFFFFF;
+            border-radius: 20px;
+            z-index: 9;
+
+        }
+
+        .center2 {
+            min-width: 28%;
+            height: 20%;
+            position: absolute;
+            top: 75%;
+            left: 413px;
+            background-color: #FFFFFF;
+            border-radius: 20px;
+            z-index: 9;
+
+        }
+
+        .center3 {
+            min-width: 28%;
+            height: 20%;
+            position: absolute;
+            top: 75%;
+            left: 788px;
+            background-color: #FFFFFF;
+            border-radius: 20px;
+            z-index: 9;
+
+        }
+
+        .isi {
+            height: 500px;
+            width: 100%;
+            display: flex;
+            justify-content: center;
+            align-items: center;
+            text-align: center;
+            font-family: poppins;
+        }
+
+        .card {
+            margin: 10px;
+            height: 250px;
+            border-top: 0px;
+            box-shadow: 0 2px 2px 0px;
+            margin-top: 0px;
+            width: 25rem;
+            border-radius: 20px 20px 20px 20px;
+        }
+
+        .btn {
+            border-radius: 5px 5px 5px 5px;
+            background-color: white;
+            border: none;
+            color: black;
+        }
+
+        .atas {
+            background-color: #150544;
+            height: 140px;
+            width: 235px;
+            border-radius: 15px;
+            margin-top: 2905px;
+            z-index: 15;
+            top: 75%;
+            position: absolute;
+            margin-left: 80px;
+        }
+
+        .bawah {
+            background-color: #150544;
+            height: 160px;
+            width: 140px;
+            border-radius: 15px;
+            margin-top: 2545px;
+            z-index: 15;
+            top: 75%;
+            position: absolute;
+            margin-left: 450px;
+        }
+
+
+        /* css cuman buat biar ke tengah ajaa */
+    </style>
+
+</head>
+
+<!-- MENU BAR -->
+<nav class="navbar navbar-expand-lg" style="margin-top: -10px;">
+
+    <div class="container" style="width: 100%; ">
+        <a class="navbar-brand" href="#" style="padding: 15px; margin-top: -10px; margin-left:0px;">
+            <img src="<?php echo base_url() ?>assets/images/ini-inti1.png" class="" alt="logo umprop inti" width="75">
+        </a>
+
+        <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+            <span class="navbar-toggler-icon"></span>
+        </button>
+
+        <ul class="navbar-nav ml-auto" style="margin-top:25px;">
+            <div class="collapse navbar-collapse" id="navbarNav" style="padding-right: -60px; font-family:poppins;">
+                <ul class="navbar-nav ml-auto">
+                    <li class="nav-item">
+                        <a href="<?php echo site_url('Home'); ?>" class="nav-link" style="color: black;">Home</a>
+                    </li>
+                    <li class="nav-item">
+                        <a href="<?php echo site_url('Content/profil'); ?>" class="nav-link" style="color: black;">Profil</a>
+                    </li>
+                    <li class="nav-item">
+                        <a href="<?php echo site_url('Content/layanan'); ?>" class="nav-link" style="color: black;">Layanan INTI-CSIRT</a>
+                    </li>
+                    <li class="nav-item">
+                        <a href="<?php echo site_url(''); ?>" class="nav-link" style="color: black;">RFC 2350</a>
+                    </li>
+                    <li class="nav-item dropdown" style="margin-left:-10px;">
+                        <a class="nav-link dropdown-toggle" href="" id="navbarDropdownMenuLink" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false" style="color: black;">
+                        </a>
+                        <ul class="dropdown-menu" aria-labelledby="navbarDropdownMenuLink">
+                            <li><a class="dropdown-item" href="<?php echo site_url('Content/indok/'); ?>">Informasi Dokumen</a></li>
+                            <li><a class="dropdown-item" href="<?php echo site_url('Content/informasi/'); ?>">Informasi Kontak</a></li>
+                            <li><a class="dropdown-item" href="<?php echo site_url('Content/tentang/'); ?>">Tentang INTI-CSIRT</a></li>
+                            <li><a class="dropdown-item" href="<?php echo site_url('Content/kebijakan/'); ?>">Kebijakan</a></li>
+                            <li><a class="dropdown-item" href="<?php echo site_url('Content/dokpen/'); ?>">Dokumen Legal Pendukung</a></li>
+                            <li><a class="dropdown-item" href="<?php echo site_url('Content/alper/'); ?>">Alamat Pelaporan</a></li>
+                        </ul>
+                    </li>
+                    <li class="nav-item">
+                        <a href="<?php echo site_url('Content/siber'); ?>" class="nav-link" style="color: black;">Aduan Siber</a>
+                    </li>
+                    <li class="nav-item">
+                        <a href="<?php echo site_url('Content/hubungi'); ?>" class="nav-link" style="color: black;">Hubungi Kami</a>
+                    </li>
+                </ul>
+            </div>
+        </ul>
+    </div>
+</nav>
+
+<!-- HERO -->
+<section class="" style="width:100%; height: 600px;">
+    <div class="container" style="background-color: #150544; width:960px; height:510px; border-top-right-radius: 40px; margin-right:110px; margin-top:80px;">
+        <h2 style="border-style: solid; width:18%; padding: 20px; border-radius:20px; color:white; margin-left:100px; margin-top:35px;"> BULETIN</h2>
+        <p style="color: white; font-family:poppins; margin: right 50%; margin-left:45%; margin-top:60px;"><img src="<?php echo base_url() ?>assets/images/berita.png" style="width:80%; margin-left: -80% ;margin-top:-90px;">
+        </p>
+        <p style="color: white; font-family:poppins; margin: right 50%; margin-left:50%; margin-top: -32%;">Apakah Lorem Ipsum itu?
+            <br>
+            <br>
+            Lorem Ipsum adalah contoh teks atau dummy dalam industri percetakan dan penataan huruf atau typesetting.
+            Lorem Ipsum telah menjadi standar contoh teks sejak tahun 1500an, saat seorang tukang cetak yang tidak dikenal
+            mengambil sebuah kumpulan teks dan mengacaknya untuk menjadi sebuah buku contoh huruf. Ia tidak hanya bertahan
+            selama 5 abad, tapi juga telah beralih ke penataan huruf elektronik, tanpa ada perubahan apapun. Ia mulai
+            dipopulerkan pada tahun 1960 dengan diluncurkannya lembaran-lembaran Letraset yang menggunakan kalimat-kalimat
+            dari Lorem Ipsum, dan seiring munculnya perangkat lunak Desktop Publishing seperti Aldus PageMaker juga memiliki
+            versi Lorem Ipsum.
+        </p>
+    </div>
+</section>
+<section class="">
+    <div class="container col-12">
+        <p style="color:#150544; font-family: poppins; margin-left: 55px; margin-top:20px;">NEWS</p>
+        <h3 style="color:#150544; font-family: poppins; margin-left: 55px; margin-top:20px;"><b>news update for you</b></h3>
+        <div class="col-md-12">
+            <form action="https://goo.gl/maps/DMUmoieZ1eRAvnG2A">
+                <button type="submit" class="btn btn-labeled" style="color: #656565; margin-left: 1000px; margin-top:-90px; background-color:#F2F2F2; width:200px; font-family: poppins; height:40px; border-radius:5px;">
+                    <i class="fa-solid fa-location-dot"></i> Bandung, Indonesia<span class="btn-label"><i class="fa fa-chevron-down"></i></span>
+                </button>
+            </form>
+        </div>
+        <div class="fill">
+            <div class="container" style="border-radius: 20px; position: relative;">
+                <div class="center">
+                    <p style="font-family: poppins; margin-top:20px; font-size:11px; margin-left:20px;"><b>Kerentanan Zero-Day pada Apache<br>Java Logging Library LOG4J</b>
+                        <button type="submit" class="btn btn-labeled" style="background-color: #23BBD1; color:white; font-family: poppins; margin-left:86px; border-radius:12px; margin-top:-5px;">
+                            <span class="btn-label"><i class="fa-solid fa-arrow-right"></i></span></button>
+                    </p>
+                </div>
+                <div class="center2">
+                    <p style="font-family: poppins; margin-top:20px; font-size:11px; margin-left:20px;"><b>Kerentanan Zero-day Windows 11 :<br> Membuat Siapapun Menjadi Admin</b>
+                        <button type="submit" class="btn btn-labeled" style="background-color: #23BBD1; color:white; font-family: poppins; margin-left:42px; border-radius:12px; margin-top:-5px;">
+                            <span class="btn-label"><i class="fa-solid fa-arrow-right"></i></span></button>
+                    </p>
+                </div>
+                <div class="center3">
+                    <p style="font-family: poppins; margin-top:20px; font-size:11px; margin-left:20px;"><b>Tips: Membuat Strong Password <br> (Kata Sandi Yang Aman)</b>
+                        <button type="submit" class="btn btn-labeled" style="background-color: #23BBD1; color:white; font-family: poppins; margin-left:102px; border-radius:12px; margin-top:-5px;">
+                            <span class="btn-label"><i class="fa-solid fa-arrow-right"></i></span></button>
+                    </p>
+                </div>
+                <img src="<?php echo base_url() ?>assets/images/berita1.jpg" class="rounded-4" alt="" style="width: 350px; height: 350px; margin-left: 10px; border-radius: 25px; margin-top:0px;">
+                <img src="<?php echo base_url() ?>assets/images/berita2.jpg" class="rounded-4" alt="" style="width: 350px; height: 350px; margin-left: 20px; border-radius: 25px; margin-top:0px;">
+                <img src="<?php echo base_url() ?>assets/images/berita3.jpg" class="rounded-4" alt="" style="width: 350px; height: 350px; margin-left: 22px; border-radius: 25px; margin-top:0px;">
+            </div>
+            <div class="container mt-4" style="border-radius: 20px; position: relative;">
+                <div class="center">
+                    <p style="font-family: poppins; margin-top:20px; font-size:11px; margin-left:20px;"><b>Kerentanan Zero-Day pada Apache<br>Java Logging Library LOG4J</b>
+                        <button type="submit" class="btn btn-labeled" style="background-color: #23BBD1; color:white; font-family: poppins; margin-left:86px; border-radius:12px; margin-top:-5px;">
+                            <span class="btn-label"><i class="fa-solid fa-arrow-right"></i></span></button>
+                    </p>
+                </div>
+                <div class="center2">
+                    <p style="font-family: poppins; margin-top:20px; font-size:11px; margin-left:20px;"><b>Kerentanan Zero-day Windows 11 :<br> Membuat Siapapun Menjadi Admin</b>
+                        <button type="submit" class="btn btn-labeled" style="background-color: #23BBD1; color:white; font-family: poppins; margin-left:42px; border-radius:12px; margin-top:-5px;">
+                            <span class="btn-label"><i class="fa-solid fa-arrow-right"></i></span></button>
+                    </p>
+                </div>
+                <div class="center3">
+                    <p style="font-family: poppins; margin-top:20px; font-size:11px; margin-left:20px;"><b>Tips: Membuat Strong Password <br> (Kata Sandi Yang Aman)</b>
+                        <button type="submit" class="btn btn-labeled" style="background-color: #23BBD1; color:white; font-family: poppins; margin-left:102px; border-radius:12px; margin-top:-5px;">
+                            <span class="btn-label"><i class="fa-solid fa-arrow-right"></i></span></button>
+                    </p>
+                </div>
+                <img src="<?php echo base_url() ?>assets/images/berita1.jpg" class="rounded-4" alt="" style="width: 350px; height: 350px; margin-left: 10px; border-radius: 25px; margin-top:0px;">
+                <img src="<?php echo base_url() ?>assets/images/berita2.jpg" class="rounded-4" alt="" style="width: 350px; height: 350px; margin-left: 20px; border-radius: 25px; margin-top:0px;">
+                <img src="<?php echo base_url() ?>assets/images/berita3.jpg" class="rounded-4" alt="" style="width: 350px; height: 350px; margin-left: 22px; border-radius: 25px; margin-top:0px;">
+            </div>
+            <div class="container mt-4" style="border-radius: 20px; position: relative;">
+                <div class="center">
+                    <p style="font-family: poppins; margin-top:20px; font-size:11px; margin-left:20px;"><b>Kerentanan Zero-Day pada Apache<br>Java Logging Library LOG4J</b>
+                        <button type="submit" class="btn btn-labeled" style="background-color: #23BBD1; color:white; font-family: poppins; margin-left:86px; border-radius:12px; margin-top:-5px;">
+                            <span class="btn-label"><i class="fa-solid fa-arrow-right"></i></span></button>
+                    </p>
+                </div>
+                <div class="center2">
+                    <p style="font-family: poppins; margin-top:20px; font-size:11px; margin-left:20px;"><b>Kerentanan Zero-day Windows 11 :<br> Membuat Siapapun Menjadi Admin</b>
+                        <button type="submit" class="btn btn-labeled" style="background-color: #23BBD1; color:white; font-family: poppins; margin-left:42px; border-radius:12px; margin-top:-5px;">
+                            <span class="btn-label"><i class="fa-solid fa-arrow-right"></i></span></button>
+                    </p>
+                </div>
+                <div class="center3">
+                    <p style="font-family: poppins; margin-top:20px; font-size:11px; margin-left:20px;"><b>Tips: Membuat Strong Password <br> (Kata Sandi Yang Aman)</b>
+                        <button type="submit" class="btn btn-labeled" style="background-color: #23BBD1; color:white; font-family: poppins; margin-left:102px; border-radius:12px; margin-top:-5px;">
+                            <span class="btn-label"><i class="fa-solid fa-arrow-right"></i></span></button>
+                    </p>
+                </div>
+                <img src="<?php echo base_url() ?>assets/images/berita1.jpg" class="rounded-4" alt="" style="width: 350px; height: 350px; margin-left: 10px; border-radius: 25px; margin-top:0px;">
+                <img src="<?php echo base_url() ?>assets/images/berita2.jpg" class="rounded-4" alt="" style="width: 350px; height: 350px; margin-left: 20px; border-radius: 25px; margin-top:0px;">
+                <img src="<?php echo base_url() ?>assets/images/berita3.jpg" class="rounded-4" alt="" style="width: 350px; height: 350px; margin-left: 22px; border-radius: 25px; margin-top:0px;">
+            </div>
+        </div>
+    </div>
+</section>
+<footer style="background:#150544; padding:10px; text-align:center; height:100px; margin-top:500px">
+    <p style="margin-top: 28px; color:white; font-family:poppins;">Copyright &copy; 2022 INTI-CSIRT</p>
+</footer>
+</body>
+
+</html>
